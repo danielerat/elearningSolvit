8b92c8b1d258351d82a65f616f9d94ce5130c58a
danielerat <davidodo2015@gmail.com>
2023-03-09 00:00:48 +0200

    Implementing nested urls

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Course, Module, Lesson, Content
 
-# Register your models here.
+# Define the admin classes for each model
+class ModuleInline(admin.StackedInline):
+    model = Module
+    extra = 0
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 0
+
+class ContentInline(admin.StackedInline):
+    model = Content
+    extra = 0
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [ModuleInline]
+
+class ModuleAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('title', 'course', 'order')
+    list_filter = ('course',)
+    search_fields = ('title', 'description')
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [ContentInline]
+    list_display = ('title', 'module', 'order')
+    list_filter = ('module__course', 'module')
+    search_fields = ('title', 'description')
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lesson', 'order', 'content_type')
+    list_filter = ('lesson__module__course', 'lesson__module', 'lesson')
+    search_fields = ('title', 'text')
+
+# Register the admin classes for each model
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Module, ModuleAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Content, ContentAdmin)
