0888b5432d979405c3533ed575ec435d1b660ce4
danielerat <davidodo2015@gmail.com>
2023-03-10 08:33:48 +0200

    Adding Is Staff or Read only Permissions to all the views

--- course/views.py
+++ course/views.py
@@ -7,32 +7,27 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from course.filters import CourseFilter
 class IsInstructorOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow instructors to edit their courses.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
+    def has_permission(self,request,view):
+    # Read permissions are allowed to any request,
+    # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS :
             return True
-
-        # Write permissions are only allowed to the instructor of the course.
-        return obj.instructor == request.user
-
+    # Write permissions are only allowed to the instructor of the course.
+        return bool(request.user and request.user.is_staff)
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [IsInstructorOrReadOnly]
     filter_backends=[DjangoFilterBackend,SearchFilter]
     filterset_class=CourseFilter
     search_fields=['title','description','instructor__first_name','instructor__last_name']
     def get_serializer_context(self):
-        return {'instructor_id':1}
+        return {'instructor_id':self.request.user.id}
     
 class ModuleViewSet(viewsets.ModelViewSet):
     serializer_class = ModuleSerializer
-    # permission_classes = [IsInstructorOrReadOnly]
+    permission_classes = [IsInstructorOrReadOnly]
     def get_queryset(self):
         return Module.objects.filter(course_id=self.kwargs['course_pk'])
     def get_serializer_context(self):
@@ -46,7 +41,7 @@ class LessonViewSet(viewsets.ModelViewSet):
     
     def get_serializer_context(self):
         return {'module_id':self.kwargs['module_pk']}
-    # permission_classes = [IsInstructorOrReadOnly]
+    permission_classes = [IsInstructorOrReadOnly]
 
     
 
@@ -55,6 +50,6 @@ class ContentViewSet(viewsets.ModelViewSet):
     serializer_class = ContentSerializer
     def get_queryset(self):
         return Content.objects.filter(lesson_id=self.kwargs['lesson_pk'])
-    # permission_classes = [IsInstructorOrReadOnly]
+    permission_classes = [IsInstructorOrReadOnly]
     def get_serializer_context(self):
         return {'lesson_id':self.kwargs['lesson_pk']}
